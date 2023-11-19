b44382cabda2cccb1214e3bfd52960c9f96d80e8
Nazar Yaremiiy <nazar.yaremiiy@gmail.com>
2023-11-19 16:09:42 +0200

    Added modal order window, modify js file.

--- js/index.js
+++ js/index.js
@@ -3,53 +3,110 @@ const burger = document.getElementById('js-burger');
 const closeMenuIcon = document.getElementById('js-close-menu');
 const menuLinks = document.querySelectorAll('.nav__link');
 const blurScreen = document.getElementById('js-blur-screen');
+const btnOrderCall = document.querySelectorAll('.btn-order-call');
+const orderWindow = document.getElementById('js-order-window');
+const closeOrderWindow = document.getElementById('js-close-order-window');
 
-function openMenu() {
+/**
+ * Функція спрацьовує при кліку на іконку бургера.
+ *
+ */
+function openMenuBurger() {
   burger.addEventListener('click', () => {
-    toggleMenuClass();
-    toggleBlurScreen();
-    toggleScrollWindow();
+    toggleElements();
   });
 }
 
-function closeMenu() {
-  closeMenuIcon.addEventListener('click', () => {
-    toggleMenuClass();
-    toggleBlurScreen();
-    toggleScrollWindow();
-  });
-}
+openMenuBurger();
 
-function toggleMenuClass() {
-  menu.classList.toggle('menu--open');
-}
-
-function toggleBlurScreen() {
-  blurScreen.classList.toggle('blur-screen--active');
-}
-
-menuLinks.forEach(link => {
-  link.addEventListener('click', () => {
-    toggleMenuClass();
-    toggleBlurScreen();
-    toggleScrollWindow();
-  });
-});
-
+/**
+ * Функція додає до тегу body клас 'noscroll', який видаляє скрол на сторінці.
+ */
 function toggleScrollWindow() {
   document.body.classList.toggle('noscroll');
 }
 
-blurScreen.addEventListener('click', () => {
-  toggleMenuClass();
-  toggleBlurScreen();
-  toggleScrollWindow();
-});
-blurScreen.addEventListener('touchend', () => {
-  toggleMenuClass();
-  toggleBlurScreen();
-  toggleScrollWindow();
+/**
+ * Функція додає та видаляє заблюрений екран.
+ */
+function toggleBlurScreen() {
+  blurScreen.classList.toggle('blur-screen--active');
+}
+
+/**
+ * Функція, яка при кліку або свайпі по заблюреному вікні закриває його та закриває меню.
+ */
+function closeAllElementInPage() {
+  blurScreen.addEventListener('touchend', () => {
+    toggleElements();
+  });
+}
+
+closeAllElementInPage();
+
+/**
+ * Функція додає та видаляє додатковий клас відкриття для меню.
+ */
+function toggleMenuClass() {
+  menu.classList.toggle('menu--open');
+}
+
+/**
+ * Функція закриває меню та блюр на екрані, спрацьовує при натисканні на хрестик закриття.
+ */
+function closeMenu() {
+  closeMenuIcon.addEventListener('click', () => {
+    toggleElements();
+  });
+}
+
+closeMenu();
+
+/**
+ * Функція закриває меню та блюр і переходить за посиланням на яке було натиснуто.
+ */
+menuLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    toggleElements();
+  });
 });
 
-openMenu();
-closeMenu();
+/**
+ * Toggle функція
+ */
+function toggleElements() {
+  toggleScrollWindow();
+  toggleBlurScreen();
+  toggleMenuClass();
+}
+
+/** 
+ * Функція відкриває модальне вікно для замовлення.
+ */
+function openModalOrderWindow() {
+  btnOrderCall.forEach(btn => {
+    btn.addEventListener('click', () => {
+      toggleModalWindowOrder();
+    });
+  })
+}
+
+openModalOrderWindow();
+
+/**
+ * Функція видаляє та додає клас до модального вікна.
+ */
+function toggleModalWindowOrder () {
+  orderWindow.classList.toggle('w-call-order--open')
+}
+
+/**
+ * Функція закриває модальне вікнот замовлення.
+ */
+function closeModalWindowOrder() {
+  closeOrderWindow.addEventListener('click', () => {
+    toggleModalWindowOrder();
+  })
+}
+
+closeModalWindowOrder();
