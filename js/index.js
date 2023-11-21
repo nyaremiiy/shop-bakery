721d8ad7969fad1f004776809d9824307df69252
Nazar Yaremiiy <nazar.yaremiiy@gmail.com>
2023-11-21 21:53:17 +0200

    Connected Swiper CDN and added slider to page.

--- js/index.js
+++ js/index.js
@@ -151,3 +151,35 @@ function showSucceedMessage() {
     messageSucceed.classList.remove('call-accepted--show');
   }, 2500);
 }
+
+// Swiper
+new Swiper('.swiper', {
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper__button-next',
+    prevEl: '.swiper__button-prev',
+  },
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  spaceBetween: 20,
+  autoHeight: true,
+  slidesPerView: 1,
+  breakpoints: {
+    320: {
+      slidesPerView: 1.2,
+    },
+    480: {
+      slidesPerView: 1.7,
+    },
+    768: {
+      slidesPerView: 2.5,
+    },
+    1279: {
+      slidesPerView: 3,
+    },
+  },
+
+
+});
